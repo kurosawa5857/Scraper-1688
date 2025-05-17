3287d1692935634fde51336be9f5d8a37c5fb5ba
Kurosawa Atsushi <endeavour0507@yahoo.co.jp>
2025-05-17 21:20:27 +0900

    Render用にPORTバインド対応などを追加

--- app.py
+++ app.py
@@ -72,3 +72,8 @@ def scrape_1688_with_playwright(url):
 
         browser.close()
         return filename
+    
+    if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
